e4616007659023d518a5b7d323172f5de7cd14a4
Andrzej Senderecki <33809996+andrzejsenderecki@users.noreply.github.com>
2017-12-02 18:57:14 +0100

    Update get-1-million-euros-object-oriented-programming-version.py

--- get-1-million-euros-object-oriented-programming-version.py
+++ get-1-million-euros-object-oriented-programming-version.py
@@ -1,6 +1,6 @@
 import random
-class QuestionEasy:
 
+class QuestionEasy:
     def __init__(self,quest,answerA,answerB,answerC,answerD,answerGood):
         self.quest = quest
         self.answerA = answerA
@@ -8,21 +8,125 @@ class QuestionEasy:
         self.answerC = answerC
         self.answerD = answerD
         self.answerGood = answerGood
-        self.fullGuestion = quest,answerA,answerB,answerC,answerD,answerGood
+        self.fullQuestion = quest,answerA,answerB,answerC,answerD,answerGood
+
+class QuestionMedium(QuestionEasy):
+    def __init__(self,quest,answerA,answerB,answerC,answerD,answerGood):
+        super().__init__(quest,answerA,answerB,answerC,answerD,answerGood)
+
+class QuestionHard(QuestionEasy):
+    def __init__(self,quest,answerA,answerB,answerC,answerD,answerGood):
+        super().__init__(quest,answerA,answerB,answerC,answerD,answerGood)
 
 class RandomQuestion:
-    def createListRandom():
-        global questRandom
-        listQuestRandom = [questionEasy1.fullGuestion,questionEasy2.fullGuestion,questionEasy3.fullGuestion]
-        questRandom = random.choice(listQuestRandom)
-        print(questRandom[0])
-        print(questRandom[1])
-        print(questRandom[2])
-        print(questRandom[3])
-        print(questRandom[4])
+    def createListQuestionEasy():
+        global listQuestRandomEasy
+        listQuestRandomEasy = [questionEasy1.fullQuestion, questionEasy2.fullQuestion, questionEasy3.fullQuestion]
+
+    def createListQuestionMedium():
+        global listQuestRandomMedium
+        listQuestRandomMedium = [questionMedium1.fullQuestion, questionMedium2.fullQuestion, questionMedium3.fullQuestion]
+
+    def createListQuestionHard():
+        global listQuestRandomHard
+        listQuestRandomHard = [questionHard1.fullQuestion, questionHard2.fullQuestion, questionHard3.fullQuestion]
+
+    def questionRandomEasy():
+        global questRandomEasy
+        questRandomEasy = random.choice(listQuestRandomEasy)
+        print(questRandomEasy[0])
+        print(questRandomEasy[1])
+        print(questRandomEasy[2])
+        print(questRandomEasy[3])
+        print(questRandomEasy[4])
+
+    def questionRandomMedium():
+        global questRandomMedium
+        questRandomMedium = random.choice(listQuestRandomMedium)
+        print(questRandomMedium[0])
+        print(questRandomMedium[1])
+        print(questRandomMedium[2])
+        print(questRandomMedium[3])
+        print(questRandomMedium[4])
+
+    def questionRandomHard():
+        global questRandomHard
+        questRandomHard = random.choice(listQuestRandomHard)
+        print(questRandomHard[0])
+        print(questRandomHard[1])
+        print(questRandomHard[2])
+        print(questRandomHard[3])
+        print(questRandomHard[4])
+
+    def RemoveQuestionEasy():
+        if questRandomEasy == questionEasy1.fullQuestion:
+            listQuestRandom.remove(questionEasy1.fullQuestion)
+            print(listQuestRandom)
+        elif questRandomEasy == questionEasy2.fullQuestion:
+            listQuestRandomEasy.remove(questionEasy2.fullQuestion)
+            print(listQuestRandomEasy)
+        elif questRandomEasy == questionEasy3.fullQuestion:
+            listQuestRandomEasy.remove(questionEasy3.fullQuestion)
+            print(listQuestRandomEasy)
+
+    def RemoveQuestionMedium():
+        if questRandomMedium == questionMedium1.fullQuestion:
+            listQuestRandomMedium.remove(questionMedium1.fullQuestion)
+            print(listQuestRandomMedium)
+        elif questRandomMedium == questionMedium2.fullQuestion:
+            listQuestRandomMedium.remove(questionMedium2.fullQuestion)
+            print(listQuestRandomMedium)
+        elif questRandomMedium == questionMedium3.fullQuestion:
+            listQuestRandomMedium.remove(questionMedium3.fullQuestion)
+            print(listQuestRandomMedium)
+
+    def RemoveQuestionHard():
+        if questRandomHard == questionHard1.fullQuestion:
+            listQuestRandomHard.remove(questionHard1.fullQuestion)
+            print(listQuestRandomHard)
+        elif questRandomHard == questionHard2.fullQuestion:
+            listQuestRandomHard.remove(questionHard2.fullQuestion)
+            print(listQuestRandomHard)
+        elif questRandomHard == questionHard3.fullQuestion:
+            listQuestRandomHard.remove(questionHard3.fullQuestion)
+            print(listQuestRandomHard)
+
+class CheckAnswer:
+    def createVaribleScoreandCash():
+        global score
+        global cash
+        score = 0
+        cash = 0
+
+    def checkPlayerAnswer():
+        global score
+        if answerPlayer == questRandomEasy[5]:
+            print('Good answer!')
+            score += 1
+            startScore = CheckAnswer
+            startScore.score()
+        elif answerPlayer == questRandomMedium[5]:
+            print('Good answer!')
+            score += 1
+            startScore = CheckAnswer
+            startScore.score()
+        elif answerPlayer == questRandomHard[5]:
+            print('Good answer!')
+            score += 1
+            startScore = CheckAnswer
+            startScore.score()
+        else:
+            print('Bad answer!')
+
+    def score():
+        if score == 1:
+            cash = 500
+            print('You have 500 Euros!')
+        elif score == 2:
+            cash = 1000
+            print('You Have 1000 Euros!')
 
 class Lifebuoy():
-
     def lifebuoy5050():
         listFullQuestion = questRandom
         listLifebuoy5050 = [listFullQuestion[1],listFullQuestion[2],listFullQuestion[3],listFullQuestion[4],listFullQuestion[5]]
@@ -61,7 +165,6 @@ class Lifebuoy():
 
     def lifebuoyPhone():
         phoneVariantRandom = [1,2,3]
-
         phoneResultRandom = random.choice(phoneVariantRandom)
 
         if phoneResultRandom == 1:
@@ -147,12 +250,66 @@ class Lifebuoy():
 questionEasy1 = QuestionEasy('What city is the capital of England?', 'a - London', 'b - Moscow', 'c - Prague', 'd - Paris', 'a - London')
 questionEasy2 = QuestionEasy('What was the name of the Greek goddess of love?', 'a - Athena', 'b - Aphrodite', 'c - Euterpe', 'd - Hecate', 'b - Aphrodite')
 questionEasy3 = QuestionEasy('Where will the Mundial 2018?', 'a - Germany', 'b - France', 'c - Russia', 'd - England', 'c - Russia')
+
+questionMedium1 = QuestionMedium('Where is Albania?', 'a - Europe', 'b - Asia', 'c - Africa', 'd - South America', 'a - Europe')
+questionMedium2 = QuestionMedium('The light on the North Pole?', 'a - Rainbow', 'b - Aurora borealis', 'c - Glow', 'd - Mirage', 'b - Aurora borealis')
+questionMedium3 = QuestionMedium('What is the name of the power unit', 'a - Decibel', 'b - Volt', 'c - Watt', 'd - Boron', 'c - Watt')
+
+questionHard1 = QuestionHard('Wchich Superhero drew Bob Kane?', 'a - Batman', 'b - Spiderman', 'c - Captain America', 'd - Superman', 'a - Batman')
+questionHard2 = QuestionHard('How years ago sank The Titanic?', 'a - 1911', 'b - 1912', 'c - 1913', 'd - 1914', 'b - 1912')
+questionHard3 = QuestionHard('What animal is particulary treat in China?', 'a - Tiger', 'b - Elephant', 'c - Panda', 'd - Bear', 'c - Panda')
+
 lifebuoyAll = Lifebuoy
-printLIST = RandomQuestion
-printLIST.createListRandom()
+printList = RandomQuestion
+checkAnswer = CheckAnswer
+checkAnswer.createVaribleScoreandCash()
+printList.createListQuestionEasy()
+printList.createListQuestionMedium()
+printList.createListQuestionHard()
+
+printList.questionRandomEasy()
 print('You write good answer: a, b, c or d')
 print('Do you want use lifebuoy?')
-lifebuoyAll.lifebuoy5050()
-lifebuoyAll.lifebuoyPhone()
-lifebuoyAll.lifebuoyAudience()
 answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionEasy()
+
+
+printList.questionRandomEasy()
+print('You write good answer: a, b, c or d')
+print('Do you want use lifebuoy?')
+answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionEasy()
+
+printList.questionRandomMedium()
+print('You write good answer: a, b, c or d')
+print('Do you want use lifebuoy?')
+answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionMedium()
+
+printList.questionRandomMedium()
+print('You write good answer: a, b, c or d')
+print('Do you want use lifebuoy?')
+answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionMedium()
+
+printList.questionRandomHard()
+print('You write good answer: a, b, c or d')
+print('Do you want use lifebuoy?')
+answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionHard()
+
+printList.questionRandomHard()
+print('You write good answer: a, b, c or d')
+print('Do you want use lifebuoy?')
+answerPlayer = input()
+checkAnswer.checkPlayerAnswer()
+printList.RemoveQuestionHard()
+
+# lifebuoyAll.lifebuoy5050()
+# lifebuoyAll.lifebuoyPhone()
+# lifebuoyAll.lifebuoyAudience()
